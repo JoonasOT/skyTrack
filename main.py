62fa02d0e503df40b32c073fb38aa95cb9070a8d
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2024-11-12 15:51:44 +0200

    Added featres to map

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from src.planes import getPlanes
-from src.map import getMap
+from src.map import getMap, featureAtLatLon
 from src.country import getCountry
 
 
 def main():
     country = getCountry("FI")
     print(country)
-    print(getPlanes(country))
-    print(getMap(country))
+    planes = getPlanes(country)
+    print(planes)
+    print(getMap(country,  [featureAtLatLon(plane.latitude, plane.longitude) for plane in planes.states]))
 
 
 if __name__ == "__main__":
--- src/map.py
+++ src/map.py
@@ -7,11 +7,22 @@ from src.country import Country
 assert mapbox.Geocoder().session.params['access_token'] == os.environ['MAPBOX_ACCESS_TOKEN']
 
 
-def getMap(country: Country) -> int:
+def featureAtLatLon(lat: float, lon: float):
+    return {
+        'type': 'Feature',
+        'properties': {},
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [lon, lat]
+        }
+    }
+
+
+def getMap(country: Country, features=None) -> int:
     print(F"Lon & lat: {(country.lon(), country.lat())}")
     response = mapbox.StaticStyle().image(username="mapbox", style_id="dark-v10",
                                           lon=country.lon(), lat=country.lat(), zoom=4.8, pitch=0.0,
-                                          width=720, height=1080
+                                          width=720, height=1080, features=features
                                           )
     if "image" in response.headers["Content-Type"]:
         with open("./data/map.png", "wb") as img:
